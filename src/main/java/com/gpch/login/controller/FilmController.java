5659ca4408077856e98cb06c3b07bab3d8c39544
Anatoly <toljaserg7@mail.ru>
2021-09-08 23:17:33 +0300

    not working button

--- src/main/java/com/gpch/login/controller/FilmController.java
+++ src/main/java/com/gpch/login/controller/FilmController.java
@@ -1,13 +1,15 @@
 package com.gpch.login.controller;
 
 import com.gpch.login.model.Film;
+import com.gpch.login.model.User;
 import com.gpch.login.service.FilmService;
+import com.gpch.login.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,12 +17,23 @@ import java.util.List;
 public class FilmController {
     @Autowired
     private FilmService filmService;
+    private UserService userService;
 
     @GetMapping(value="/user/films")
     public String login(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         List<Film> listFilm = filmService.findAll();
         model.addAttribute("listFilm", listFilm);
+        return "film_list_test";
+    }
+
+    @RequestMapping("/user/add_to_watchlist/{id}")
+    public String addToWatchlist(@RequestParam(value = "id") Long id, Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByUserName(auth.getName());
+        Film film = filmService.findById(id);
+        userService.addWatchedFilm(user, film);
+        model.addAttribute("user", user);
+        model.addAttribute("film", film);
         return "film_list_test";
     }
 }
